856b54e390cf4f484644dc87675958ca3c7feba0
jimancio <ihourquescos@gmail.com>
2020-09-10 10:35:04 -0300

    camvioe sen lista de preicos

--- views/js/listaEspecificaService.js
+++ views/js/listaEspecificaService.js
@@ -60,3 +60,31 @@ function myFunction() {
 
 
 obtenerListaEspecifica(id);
+
+
+// var $table = $('#myTable')
+
+//   $(function() {
+//       $table.bootstrapTable('destroy').bootstrapTable({
+//         exportDataType: $(this).val(),
+//         exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel', 'pdf'],
+//         columns: [
+//           {
+//             field: 'state',
+//             checkbox: true,
+//             visible: $(this).val() === 'selected'
+//           },
+//           {
+//             field: 'id',
+//             title: 'ID'
+//           }, {
+//             field: 'name',
+//             title: 'Item Name'
+//           }, {
+//             field: 'price',
+//             title: 'Item Price'
+//           }
+//         ]
+//       })
+//     }).trigger('change')
+
