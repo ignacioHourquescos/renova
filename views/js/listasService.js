5436ff898fbb6bd7058101f10f0217f20f66af3f
jimancio <ihourquescos@gmail.com>
2020-09-15 13:27:23 -0300

    ofertas funcionando

--- views/js/listasService.js
+++ views/js/listasService.js
@@ -4,19 +4,12 @@
 var server ="https://renovaapi.herokuapp.com";
 
 function cargarListas () {
-	//Carga las cards de las listas de precios
 	$.getJSON(server+"/listas", function (data) {
-	// Se carga la información obtenida en el DOM
 		var cantidad = data.length;
 		var idColor = 1;
 		for (i = 0; i < cantidad; i++) {
-			// Se clona la plantilla y la nueva copia ya no es una plantilla
 			var divCompetencia = $(".competenciaPlantilla").clone().removeClass("competenciaPlantilla");
-			// Se coloca el id correcto (data[i].id) de cada competencia en los links de acciones
-			$(divCompetencia).find('.link').each( function(){
-				$( this ).attr("href",$( this ).attr("href")+data[i].codigo);
-			});
-			// Se coloca el nombre de cada competencia
+			$(divCompetencia).find('.link').each( function(){$( this ).attr("href",$( this ).attr("href")+data[i].codigo);});
 			$(divCompetencia).find('.titulo').text(data[i].descripcion);
 			$(divCompetencia).find('.card').addClass('color'+idColor); 
 			$(".competencias").append(divCompetencia);
