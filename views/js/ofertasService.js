1e9b1ead68990158720fe5b72552f5ec72c0550e
jimancio <ihourquescos@gmail.com>
2020-09-22 15:05:51 -0300

    ofertas post vista vendedores

--- views/js/ofertasService.js
+++ views/js/ofertasService.js
@@ -1,7 +1,7 @@
 // Al finalizarse de cargar el DOM:
 //var port = process.env.PORT || 8080;
-var server ="https://renovaapi.herokuapp.com";
-//var server ="http://localhost:5000";
+//var server ="https://renovaapi.herokuapp.com";
+var server ="http://localhost:5000";
 
 
 
@@ -14,7 +14,7 @@ function cargarOfertasMensuales () {
 			var divCompetencia = $(".oferta").clone().removeClass("oferta");
     	  	$(divCompetencia).find('.titulo').text(data[i].id);
 		  	$(divCompetencia).find('.detalle').text(data[i].d);
-			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0).toFixed(0));
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21).toFixed(0));
 			$(divCompetencia).find('.imagenOferta').attr("src",data[i].img);
     	  	$(".ofertas").append(divCompetencia);
 		};
@@ -30,7 +30,7 @@ function cargarKits () {
 			var divCompetencia = $(".kit").clone().removeClass("kit");
     	  	$(divCompetencia).find('.titulo').text(data[i].id);
 		  	$(divCompetencia).find('.detalle').text(data[i].d);
-			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0).toFixed(0));
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21).toFixed(0));
 			$(divCompetencia).find('.imagenKit').attr("src",data[i].img);
     	  	$(".kits").append(divCompetencia);
 		};
@@ -45,7 +45,7 @@ function cargarValvoline () {
 			var divCompetencia = $(".valvoline").clone().removeClass("valvoline");
     	  	$(divCompetencia).find('.titulo').text(data[i].id);
 		  	$(divCompetencia).find('.detalle').text(data[i].d);
-			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9*0).toFixed(0));
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9).toFixed(0));
 			$(divCompetencia).find('.imagenKit').attr("src",data[i].img);
     	  	$(".valvolineOfertas").append(divCompetencia);
 		};
@@ -61,11 +61,43 @@ function cargarMotul() {
 			var divCompetencia = $(".motul").clone().removeClass("motul");
     	  	$(divCompetencia).find('.titulo').text(data[i].id);
 		  	$(divCompetencia).find('.detalle').text(data[i].d);
-			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9*0).toFixed(0));
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9).toFixed(0));
 			$(divCompetencia).find('.imagenKit').attr("src",data[i].img);
     	  	$(".motulOfertas").append(divCompetencia);
 		};
 		$("#plantilla4").remove();
+	});
+}
+
+
+function cargarTotal() {
+	//busca en el backend todas las ofertas
+	$.getJSON(server+"/ofertasTotal", function (data) {
+		for (i = 0; i < data.length; i++) {
+			var divCompetencia = $(".total").clone().removeClass("total");
+    	  	$(divCompetencia).find('.titulo').text(data[i].id);
+		  	$(divCompetencia).find('.detalle').text(data[i].d);
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9).toFixed(0));
+			$(divCompetencia).find('.imagenKit').attr("src",data[i].img);
+    	  	$(".totalOfertas").append(divCompetencia);
+		};
+		$("#plantilla5").remove();
+	});
+}
+
+
+function cargarSelenia() {
+	//busca en el backend todas las ofertas
+	$.getJSON(server+"/ofertasSelenia", function (data) {
+		for (i = 0; i < data.length; i++) {
+			var divCompetencia = $(".selenia").clone().removeClass("selenia");
+    	  	$(divCompetencia).find('.titulo').text(data[i].id);
+		  	$(divCompetencia).find('.detalle').text(data[i].d);
+			$(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9).toFixed(0));
+			$(divCompetencia).find('.imagenKit').attr("src",data[i].img);
+    	  	$(".seleniaOfertas").append(divCompetencia);
+		};
+		$("#plantilla6").remove();
 	});
 }
 
@@ -75,4 +107,7 @@ cargarOfertasMensuales();
 cargarKits(); 
 cargarValvoline();
 cargarMotul();
+cargarTotal();
+cargarSelenia();
+
 //cargarOfertasFram();
