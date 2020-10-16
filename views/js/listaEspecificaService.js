d485e35b4092541598ab5dd1ed31419261a030ac
jimancio <ihourquescos@gmail.com>
2020-10-16 10:31:18 -0300

    sticky search box

--- views/js/listaEspecificaService.js
+++ views/js/listaEspecificaService.js
@@ -125,19 +125,3 @@ obtenerListaEspecifica(id);
 document.getElementsByClassName("search-input").placeholder="buscar";
 
 
-// var fixmeTop = document.getElementsByClassName('search-input').offset().top;       // get initial position of the element
-// $(window).scroll(function() {                  // assign scroll event listener
-//     var currentScroll = $(window).scrollTop(); // get current position
-//     if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-//         $document.getElementsByClassName('search-input').css({                      // scroll to that element or below it
-//             position: 'fixed',
-//             top: '0',
-//             left: '0'
-//         });
-//     } else {                                   // apply position: static
-//         document.getElementsByClassName('search-input').css({                      // if you scroll above it
-//             position: 'static'
-//         });
-//     }
-
-// });
