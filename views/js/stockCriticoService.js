368b8c4710c3e5c0aad23f0ae6793ec2a40abc6d
jimancio <ihourquescos@gmail.com>
2021-01-28 10:49:55 -0300

    stock

--- views/js/stockCriticoService.js
+++ views/js/stockCriticoService.js
@@ -4,22 +4,30 @@ var server ="https://renovaapi.herokuapp.com";
 
 
 function obtenerListaArticulosBajoStock () {
-	$(document).ready($.getJSON(server+"/stock", function (data) { 
+	$(document).ready($.getJSON(server+"/stock", function (data) {
+		console.log(data);
 		for(i=0; i<data.length;i++){
 			let unidadesVendidasPorMes= data[i].unidades*1.3;
 			console.log(unidadesVendidasPorMes);
 			if (data[i].cant_stock==0){
 				data[i].cant_stock=0;
 			}
-			data[i].stockCritico=unidadesVendidasPorMes/data[i].cant_stock;
+			
+			data[i].stockCritico=(unidadesVendidasPorMes/data[i].cant_stock).toFixed(2);
+			// if (data[i].stockCritico >= 0){
+			// 	data[i].stockCritico ="nada";
+			// } else{data[i].stockCritico}
 			// let ventaMensual[i]=data.resultado[i].unidades*1.30;
 			// let ratioStock=(data.resultado[i].unidades.toFixed(0);
 			// data.resultado[i].pf="$"+precioConIva;
 			// data.resultado[i].p="$"+data.resultado[i].p.toFixed(0);
 			// data.resultado[i].s=convertirStockNumericoEnEscala(data.resultado[i].s);
-		}		
+			
+		}	
+
 	 $('table').bootstrapTable({ 
-		data:data
+		data:data.filter(filtrarcodigos)
+		//data:data
 		}); 
 	}));
 }
@@ -34,3 +42,9 @@ obtenerListaArticulosBajoStock();
 // "cant_stock": -1,
 // "stockCritico": -40
 
+
+
+// filtro los codigos que vael la pena contar 
+function filtrarcodigos(cantidad){
+	return((cantidad.stockCritico > 0 && cantidad.stockCritico > 0.5) || cantidad.cant_stock<0);
+}
