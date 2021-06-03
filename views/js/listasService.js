42db960d93418d569af38b5019cd1c0d6d7897c6
jimancio <ihourquescos@gmail.com>
2021-06-03 11:26:07 -0300

    modificacion cars auemnto

--- views/js/listasService.js
+++ views/js/listasService.js
@@ -1,7 +1,7 @@
 // Al finalizarse de cargar el DOM:
 
-//var server ="http://localhost:5000";
-var server ="https://renovaapi.herokuapp.com";
+var server ="http://localhost:5000";
+//var server ="https://renovaapi.herokuapp.com";
 
 function cargarListas () {
 	$.getJSON(server+"/listas", function (data) {
@@ -12,6 +12,7 @@ function cargarListas () {
 			$(divCompetencia).find('.link2').each( function(){$( this ).attr("href",$( this ).attr("href")+data[i].codigo);});
 			$(divCompetencia).find('.titulo').text(data[i].descripcion);
 			$(divCompetencia).find('.mes').text(data[i].mes);
+			$(divCompetencia).find('.mes2').text(data[i].mes2);
 			$(divCompetencia).find('.aumento').text(data[i].aumento);
 			$(divCompetencia).find('.card').addClass('color'+idColor); 
 			$(".competencias").append(divCompetencia);
