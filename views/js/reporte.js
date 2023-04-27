5fd00304fd83253006a34b9440149bead45f1f2f
unknown <ihourquescos@gmail.com>
2023-04-27 18:43:02 -0300

    cambio a heroku 20

--- views/js/reporte.js
+++ views/js/reporte.js
@@ -1,179 +1,227 @@
-var server ="https://renovaapi.herokuapp.com";
+var server = "https://renovaapi-heroku-20.herokuapp.com";
 //var server ="http://localhost:5000";
-var ventasFiltros=[];
+var ventasFiltros = [];
 
-function ventasPorAgrupacion(id,mes,codigo) {  
-    $(document).ready($.getJSON(server+"/ventasPorAgrupacion?id="+id+"&fechaDesde=2020"+mes+"01&fechaHasta=2020"+mes+"29", function (data) {         
-      
-      var sumador=0;
-      for (var i=0;i<data.length;i++){     
-        sumador=sumador+data[i].canti_kilos;
+function ventasPorAgrupacion(id, mes, codigo) {
+  $(document).ready(
+    $.getJSON(
+      server +
+        "/ventasPorAgrupacion?id=" +
+        id +
+        "&fechaDesde=2020" +
+        mes +
+        "01&fechaHasta=2020" +
+        mes +
+        "29",
+      function (data) {
+        var sumador = 0;
+        for (var i = 0; i < data.length; i++) {
+          sumador = sumador + data[i].canti_kilos;
+        }
+        ventasFiltros[codigo] = sumador;
       }
-      ventasFiltros[codigo]=sumador;
-    }));    
+    )
+  );
 }
 
 var agrupaciones = {
-  Fram:1,
-  OfertasFram:23,
-  KitsFram:16,
-  Mahle:15,
-  Rama:8,
-  Mann:9,
-  AlternativosHabitaculo:29,
-  Eam:48,
-  Valvoline:3,
-  Total:5,
-  Motul:12,
-  Petronas:316,
-  Trico:13,
-  WagnerLockheed:11,
-  Locx:14
-}
-var anio =[
-  {codigo:0,mes:"01",dias:31},
-  {codigo:1,mes:"01",dias:31},
-  {codigo:2,mes:"02",dias:29},
-  {codigo:3,mes:"03",dias:31},
-  {codigo:4,mes:"04",dias:30}, 
-  {codigo:5,mes:"05",dias:31},
-  {codigo:6,mes:"06",dias:30},
-  {codigo:7,mes:"07",dias:31},
-  {codigo:8,mes:"08",dias:31},
-  {codigo:9,mes:"09",dias:30},
-  {codigo:10,mes:"10",dias:31},
-  {codigo:11,mes:"11",dias:30},
-  {codigo:12,mes:"12",dias:31}
-]
+  Fram: 1,
+  OfertasFram: 23,
+  KitsFram: 16,
+  Mahle: 15,
+  Rama: 8,
+  Mann: 9,
+  AlternativosHabitaculo: 29,
+  Eam: 48,
+  Valvoline: 3,
+  Total: 5,
+  Motul: 12,
+  Petronas: 316,
+  Trico: 13,
+  WagnerLockheed: 11,
+  Locx: 14,
+};
+var anio = [
+  { codigo: 0, mes: "01", dias: 31 },
+  { codigo: 1, mes: "01", dias: 31 },
+  { codigo: 2, mes: "02", dias: 29 },
+  { codigo: 3, mes: "03", dias: 31 },
+  { codigo: 4, mes: "04", dias: 30 },
+  { codigo: 5, mes: "05", dias: 31 },
+  { codigo: 6, mes: "06", dias: 30 },
+  { codigo: 7, mes: "07", dias: 31 },
+  { codigo: 8, mes: "08", dias: 31 },
+  { codigo: 9, mes: "09", dias: 30 },
+  { codigo: 10, mes: "10", dias: 31 },
+  { codigo: 11, mes: "11", dias: 30 },
+  { codigo: 12, mes: "12", dias: 31 },
+];
 
-
-$(document).ready(function() {
-  $("#dropdown").on("option", "click", function() {
+$(document).ready(function () {
+  $("#dropdown").on("option", "click", function () {
     alert("Haha");
   });
 });
 // calse coderhouse toto
 
-const pedido = new Promise((resolve,reject)=>{
-  if(true){
-    resolve ("todo muy lindo");
+const pedido = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("todo muy lindo");
+  } else {
+    reject("todo feo");
   }
-  else{
-    reject("todo feo");}
-  }
-);
+});
 
-var array=[];
-var selected =document.getElementById("dropdown").value;
-
+var array = [];
+var selected = document.getElementById("dropdown").value;
 
 pedido
-.then(()  =>{console.log(selected);(ventasPorAgrupacion(selected, anio[0].mes, anio[0].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[1].mes, anio[1].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[2].mes, anio[2].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[3].mes, anio[3].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[4].mes, anio[4].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[5].mes, anio[5].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[6].mes, anio[6].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[7].mes, anio[7].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[8].mes, anio[8].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[9].mes, anio[9].codigo));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[10].mes,anio[10].codigo ));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[11].mes,anio[11].codigo ));})
-.then(()  =>{(ventasPorAgrupacion(selected, anio[12].mes,anio[12].codigo ));})
-.then(()  =>{console.log(ventasFiltros);})
-.then(()  =>{setTimeout(()=>{    
-  array=ventasFiltros.slice(1);
-  function renderizarGrafico(){
-      var options = {
-        series: [{
-        name: 'Filtros Fram',
-        data: array
-      }],
-        chart: {
-        height: 350,
-        type: 'bar',
-      },
-      plotOptions: {
-        bar: {
-          dataLabels: {
-            position: 'top', // top, center, bottom
+  .then(() => {
+    console.log(selected);
+    ventasPorAgrupacion(selected, anio[0].mes, anio[0].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[1].mes, anio[1].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[2].mes, anio[2].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[3].mes, anio[3].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[4].mes, anio[4].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[5].mes, anio[5].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[6].mes, anio[6].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[7].mes, anio[7].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[8].mes, anio[8].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[9].mes, anio[9].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[10].mes, anio[10].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[11].mes, anio[11].codigo);
+  })
+  .then(() => {
+    ventasPorAgrupacion(selected, anio[12].mes, anio[12].codigo);
+  })
+  .then(() => {
+    console.log(ventasFiltros);
+  })
+  .then(() => {
+    setTimeout(() => {
+      array = ventasFiltros.slice(1);
+      function renderizarGrafico() {
+        var options = {
+          series: [
+            {
+              name: "Filtros Fram",
+              data: array,
+            },
+          ],
+          chart: {
+            height: 350,
+            type: "bar",
           },
-        }
-      },
-      dataLabels: {
-        enabled: true,
-      
-        offsetY: -20,
-        style: {
-          fontSize: '12px',
-          colors: ["#304758"]
-        }
-      },
-      
-      xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        position: 'top',
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        crosshairs: {
-          fill: {
-            type: 'gradient',
-            gradient: {
-              colorFrom: '#D8E3F0',
-              colorTo: '#BED1E6',
-              stops: [0, 100],
-              opacityFrom: 0.4,
-              opacityTo: 0.5,
-            }
-          }
-        },
-        tooltip: {
-          enabled: true,
-        }
-      },
-      yaxis: {
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false,
-        },
-        labels: {
-          show: false,
-          formatter: function (val) {
-            return val;
-          }
-        }
-      
-      },
-      title: {
-        text: 'Fram',
-        floating: true,
-        offsetY: 330,
-        align: 'center',
-        style: {
-          color: '#444'
-        }
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                position: "top", // top, center, bottom
+              },
+            },
+          },
+          dataLabels: {
+            enabled: true,
+
+            offsetY: -20,
+            style: {
+              fontSize: "12px",
+              colors: ["#304758"],
+            },
+          },
+
+          xaxis: {
+            categories: [
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ],
+            position: "top",
+            axisBorder: {
+              show: false,
+            },
+            axisTicks: {
+              show: false,
+            },
+            crosshairs: {
+              fill: {
+                type: "gradient",
+                gradient: {
+                  colorFrom: "#D8E3F0",
+                  colorTo: "#BED1E6",
+                  stops: [0, 100],
+                  opacityFrom: 0.4,
+                  opacityTo: 0.5,
+                },
+              },
+            },
+            tooltip: {
+              enabled: true,
+            },
+          },
+          yaxis: {
+            axisBorder: {
+              show: false,
+            },
+            axisTicks: {
+              show: false,
+            },
+            labels: {
+              show: false,
+              formatter: function (val) {
+                return val;
+              },
+            },
+          },
+          title: {
+            text: "Fram",
+            floating: true,
+            offsetY: 330,
+            align: "center",
+            style: {
+              color: "#444",
+            },
+          },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
       }
-      };
-      
-      var chart = new ApexCharts(document.querySelector("#chart"), options);
-      chart.render();
-    }
-  renderizarGrafico();console.log("llegamso hasta aca");},4000)
-})
+      renderizarGrafico();
+      console.log("llegamso hasta aca");
+    }, 4000);
+  });
 
-
-
-
-
-
-  
 // const pedido2 = new Promise((resolve,reject)=>{if(true){resolve ("todo muy lindo");var array=[];}else{reject("todo feo");}});
 // var array=[];
 // pedido2
@@ -192,7 +240,7 @@ pedido
 //   .then(()  =>{(ventasPorAgrupacion(agrupaciones.Valvoline, anio[11].mes,anio[11].codigo ));})
 //   .then(()  =>{(ventasPorAgrupacion(agrupaciones.Valvoline, anio[12].mes,anio[12].codigo ));})
 //   .then(()  =>{console.log(ventasFiltros);})
-//   .then(()  =>{setTimeout(()=>{    
+//   .then(()  =>{setTimeout(()=>{
 //     array=ventasFiltros.slice(1);
 //     function renderizarGrafico(){
 //       var options = {
@@ -213,14 +261,14 @@ pedido
 //       },
 //       dataLabels: {
 //         enabled: true,
-      
+
 //         offsetY: -20,
 //         style: {
 //           fontSize: '12px',
 //           colors: ["#304758"]
 //         }
 //       },
-      
+
 //       xaxis: {
 //         categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 //         position: 'top',
@@ -259,7 +307,7 @@ pedido
 //             return val;
 //           }
 //         }
-      
+
 //       },
 //       title: {
 //         text: 'Fram',
@@ -271,17 +319,12 @@ pedido
 //         }
 //       }
 //       };
-      
+
 //       var chart = new ApexCharts(document.querySelector("#valvo"), options);
 //       chart.render();
 //       }
 //     renderizarGrafico();console.log("llegamso hasta aca");},3000)
 //   })
-
-
-
-
-
 
 // ventasFiltros.push(ventasPorAgrupacion(agrupaciones.Fram, anio[1].mes));
 // ventasFiltros.push(ventasPorAgrupacion(agrupaciones.Fram, anio[2].mes));
@@ -298,14 +341,10 @@ pedido
 // .then(()  => {ventasPorAgrupacion(agrupaciones.Fram, anio[3].mes);})
 // .then(()  => {renderizarGrafico(ventasFiltros)});
 
-
-
-
-
 // ventasPorAgrupacion(agrupaciones.Fram,anio[0].mes);
 // for (var i =1; i<=12; i++){
 //   var valor = ventasPorAgrupacion(agrupaciones.Fram,anio[i].mes);
-  
+
 // }
 
 // function calcularVentas (anio, agrupaciones,ventasPorAgrupacion){
@@ -315,5 +354,4 @@ pedido
 //   }
 // }
 
-
- var array;
+var array;
