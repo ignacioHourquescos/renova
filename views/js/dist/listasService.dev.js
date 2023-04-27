14ec31b8b3fd66c046e51b36f70eeb4413646a32
unknown <ihourquescos@gmail.com>
2023-04-27 18:42:19 -0300

    cambio a heroku 20

--- views/js/dist/listasService.dev.js
+++ views/js/dist/listasService.dev.js
@@ -2,7 +2,7 @@
 
 // Al finalizarse de cargar el DOM:
 //var server ="http://";
-var server = "https://renovaapi.herokuapp.com";
+var server = "https://renovaapi-heroku-20.herokuapp.com";
 
 function cargarListas() {
   $.getJSON(server + "/listas", function (data) {
@@ -10,15 +10,21 @@ function cargarListas() {
     var idColor = 1;
 
     for (i = 0; i < cantidad; i++) {
-      var divCompetencia = $(".competenciaPlantilla").clone().removeClass("competenciaPlantilla");
-      $(divCompetencia).find(".link2").each(function () {
-        $(this).attr("href", $(this).attr("href") + data[i].codigo);
-      });
+      var divCompetencia = $(".competenciaPlantilla")
+        .clone()
+        .removeClass("competenciaPlantilla");
+      $(divCompetencia)
+        .find(".link2")
+        .each(function () {
+          $(this).attr("href", $(this).attr("href") + data[i].codigo);
+        });
       $(divCompetencia).find(".titulo").text(data[i].descripcion);
       $(divCompetencia).find(".mes").text(data[i].mes);
       $(divCompetencia).find(".mes2").text(data[i].mes2);
       $(divCompetencia).find(".aumento").text(data[i].aumento);
-      $(divCompetencia).find(".card").addClass("color" + idColor);
+      $(divCompetencia)
+        .find(".card")
+        .addClass("color" + idColor);
       $(".competencias").append(divCompetencia);
       $(divCompetencia).show();
     }
