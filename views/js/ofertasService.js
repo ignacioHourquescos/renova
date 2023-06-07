d689ec45b15d2bc7b6e8c857c515add925d7ddcb
unknown <ihourquescos@gmail.com>
2023-06-07 13:14:23 -0300

    cambio ofertas

--- views/js/ofertasService.js
+++ views/js/ofertasService.js
@@ -134,8 +134,10 @@ function cargarTotal() {
       var divCompetencia = $(".total").clone().removeClass("total");
       $(divCompetencia).find(".titulo").text(data[i].id);
       $(divCompetencia).find(".detalle").text(data[i].d);
-      // $(divCompetencia).find('.precio').text("$"+(data[i].p*1.21*0.9).toFixed(0));
-      $(divCompetencia).find(".precio").text("Consultar");
+      $(divCompetencia)
+        .find(".precio")
+        .text("$" + (data[i].p * 1.21 * 0.9).toFixed(0));
+      // $(divCompetencia).find(".precio").text("Consultar");
       $(divCompetencia).find(".imagenKit").attr("src", data[i].img);
       $(".totalOfertas").append(divCompetencia);
     }
