46b0eedbfdb7107a0db05cab9ca0784508f3d08a
unknown <ihourquescos@gmail.com>
2023-11-01 16:44:09 -0300

    muestro wpp

--- views/js/indexService.js
+++ views/js/indexService.js
@@ -31,11 +31,7 @@ $(function () {
 		phone: "5491144493873", //WhatsApp Business phone number
 		//headerTitle: 'Escribinos!', //Popup Title
 		//popupMessage: 'Hola! en que te podemos ayudar?', //Popup Message
-<<<<<<< HEAD
-		showPopup: false, //Enables popup display
-=======
 		showPopup: true, //Enables popup display
->>>>>>> listas
 		buttonImage: '<img src="views/images/whatsappModern.png" />', //Button Image
 		//headerColor: 'crimson', //Custom header color
 		//backgroundColor: 'crimson', //Custom background button color
