780d5e3d06766f9483719b6a96adb875c696d263
ihourquescos <ihourquescos@gmail.com>
2025-04-29 15:05:15 -0300

    quito aumentos

--- views/js/listasService.js
+++ views/js/listasService.js
@@ -4,30 +4,28 @@
 var server = "https://renovaapi-heroku-20.herokuapp.com";
 
 function cargarListas() {
-  $.getJSON(server + "/listas", function (data) {
-    var cantidad = data.length;
-    var idColor = 1;
-    for (i = 0; i < cantidad; i++) {
-      var divCompetencia = $(".competenciaPlantilla")
-        .clone()
-        .removeClass("competenciaPlantilla");
-      $(divCompetencia)
-        .find(".link2")
-        .each(function () {
-          $(this).attr("href", $(this).attr("href") + data[i].codigo);
-        });
-      $(divCompetencia).find(".titulo").text(data[i].descripcion);
-      $(divCompetencia).find(".mes").text(data[i].mes);
-      $(divCompetencia).find(".mes2").text(data[i].mes2);
-      $(divCompetencia).find(".aumento").text(data[i].aumento);
-      $(divCompetencia)
-        .find(".card")
-        .addClass("color" + idColor);
-      $(".competencias").append(divCompetencia);
-      $(divCompetencia).show();
-    }
-    $(".spinner-border").remove();
-  });
+	$.getJSON(server + "/listas", function (data) {
+		var cantidad = data.length;
+		var idColor = 1;
+		for (i = 0; i < cantidad; i++) {
+			var divCompetencia = $(".competenciaPlantilla")
+				.clone()
+				.removeClass("competenciaPlantilla");
+			$(divCompetencia)
+				.find(".link2")
+				.each(function () {
+					$(this).attr("href", $(this).attr("href") + data[i].codigo);
+				});
+			$(divCompetencia).find(".titulo").text(data[i].descripcion);
+			$(divCompetencia).find(".mes").text(data[i].mes);
+			$(divCompetencia)
+				.find(".card")
+				.addClass("color" + idColor);
+			$(".competencias").append(divCompetencia);
+			$(divCompetencia).show();
+		}
+		$(".spinner-border").remove();
+	});
 }
 
 cargarListas();
