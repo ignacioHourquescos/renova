3115037336cc7e8b47cdaf98484284f0f2cfe74e
ihourquescos <ihourquescos@gmail.com>
2025-04-29 15:19:17 -0300

    quito aumentos

--- views/js/listasService.js
+++ views/js/listasService.js
@@ -17,7 +17,7 @@ function cargarListas() {
 					$(this).attr("href", $(this).attr("href") + data[i].codigo);
 				});
 			$(divCompetencia).find(".titulo").text(data[i].descripcion);
-			$(divCompetencia).find(".mes").text(data[i].mes);
+
 			$(divCompetencia)
 				.find(".card")
 				.addClass("color" + idColor);
