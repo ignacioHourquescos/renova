738dfd6ba73c332a0106acf7e43e1182acd03667
ihourquescos <ihourquescos@gmail.com>
2025-11-14 19:05:51 -0300

    redisñeo

--- views/js/indexService.js
+++ views/js/indexService.js
@@ -1,34 +1,17 @@
-const constantes = {
-	nosotros:
-		"Somos una empresa familiar y referente dentro del segmento de filtros y lubricantes.\
-  En 1976 cuando habia algunos pocos modelos de auto \
-  comenzamos a trabajar con la marca FRAM. \
-  Hoy, despues de 40 años seguimos  acompañando a nuestros\
-  clientes en un contexto donde el mundo de los repuestos cada vez e complejiza mas\
-  debido a la creciente cantidad de modelos",
-
-	tituloServicios1: "VENTA AL POR MAYOR DE FITLROS Y LUBRICANTES",
-	descServicios1:
-		"Distribución al por mayor de filtros, lubricantes, aditivos y productos afines. Atendemos estaciones de servicios,\
-   lubricentros, casa de repuestos, empresas de transportes y flotas de vehiculos",
-
-	tituloServicios2: "LINEA PESADA Y AGRO",
-	descServicios2:
-		"Atencion a la industria y afines. Filtros y lubricantes para motores, compresores y generadores.",
-
-	tituloServicios3: "INDUSTRIA",
-	descServicios3:
-		"Soluciones especializadas para la industria. Filtros y lubricantes de alta performance para maquinaria pesada, compresores industriales y equipos de gran exigencia.",
-};
-
 //WHATSAPP FUNCTION
 $(function () {
 	// Phone numbers for each section
+	//const phoneNumbers = {
+	//	transporte: "5491165106333", // HEMAN
+	//	lubricentro: "5491140565047", // LUCAS MOSTRADOR
+	//	agro: "5491144075850", // NUEVO NÚMERO
+	//	industria: "5491144075850", // NUEVO NÚMERO
+	//};
 	const phoneNumbers = {
 		transporte: "5491165106333", // HEMAN
-		lubricentro: "5491140565047", // LUCAS MOSTRADOR
-		agro: "5491144075850", // NUEVO NÚMERO
-		industria: "5491144075850", // NUEVO NÚMERO
+		lubricentro: "5491165106333", // LUCAS MOSTRADOR
+		agro: "5491165106333", // NUEVO NÚMERO
+		industria: "5491165106333", // NUEVO NÚMERO
 	};
 
 	// Create modal HTML if it doesn't exist yet
@@ -83,17 +66,3 @@ $(function () {
 		return false;
 	});
 });
-
-// FUNCTION
-
-function inserstartextos(id, element) {
-	document.getElementById(id).innerHTML = element;
-}
-
-inserstartextos("Nosotros", constantes.nosotros);
-inserstartextos("tituloServicios1", constantes.tituloServicios1);
-inserstartextos("descServicios1", constantes.descServicios1);
-inserstartextos("tituloServicios2", constantes.tituloServicios2);
-inserstartextos("descServicios2", constantes.descServicios2);
-inserstartextos("tituloServicios3", constantes.tituloServicios3);
-inserstartextos("descServicios3", constantes.descServicios3);
